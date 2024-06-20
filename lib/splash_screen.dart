843631de84f6db73964d14d5616b46abe17e02cf
Tahmid. <tahmidur.rahman.79274@gmail.com>
2024-06-20 22:19:31 +0600

    Check token before navigate

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import './screens/home/products_screen.dart';
 import './components/custom_widget/custom_logo_widget.dart';
 import './utils/color/app_color.dart';
 import './screens/auth/sign_in_screen.dart';
+import 'utils/shared_prefs_manager/shared_prefs_manager.dart';
 import 'utils/values/app_constant.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -15,11 +17,20 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const SignInScreen()),
-      );
+    Future.delayed(const Duration(seconds: 3), () async {
+      String token = await SharedPrefsManager.getToken();
+      if(token.isEmpty){
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const SignInScreen()),
+        );
+      }
+      else{
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const ProductsScreen()),
+        );
+      }
     });
   }
   @override
